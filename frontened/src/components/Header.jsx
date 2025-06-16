397d5981eaf47ab334cc5f21d683c1406a52156a
Navneet Kumar <navneetkumar991713@gmail.com>
2025-06-17 00:52:02 +0530

    hea

--- frontened/src/components/Header.jsx
+++ frontened/src/components/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import {assets} from '../assets/assets'
 
-
  const Header = () => {
   return (
-    <div  className='flex-col md:flex-row flex-wrap bg-pink-400 rounded-lg px-6 md:px-10 lg:px-20'>
+    <div  className=' flex flex-col md:flex-row flex-wrap bg-pink-400 rounded-lg px-6 md:px-10 lg:px-20'>
         {/*--leftside--*/}
-        <div className='md-w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:md-[-30px]'>
+        <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:md-[-30px]'>
 <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'> 
     Book Appointment  
     <br /> With Trusted Doctors
@@ -23,7 +22,7 @@ import {assets} from '../assets/assets'
       
           
           {/*---------right-side*/}
-       <div className=' float-right md:w-1/2 relative'>
+       <div className='md:w-1/2 relative'>
     <img className='w-full md:absolute bottom-0 h-auto rounded-lg' src={assets.header_img}/>
        </div> 
     </div>
